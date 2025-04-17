0f9745869bcc3f5c917caa9d53188143982b8eca
emredaglar <emre.daglar1@gmail.com>
2025-04-17 23:14:35 +0300

    added Mongo connections

---
+++ MongoSuperMarket/Settings/DatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace MongoSuperMarket.Settings
+{
+    public class DatabaseSettings:IDatabaseSetting
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string DiscountCollectionName { get; set; }
+        public string FeatureCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string SellingCollectionName { get; set; }
+    }
+}
---
+++ MongoSuperMarket/Settings/IDatabaseSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MongoSuperMarket.Settings
+{
+    public interface IDatabaseSetting
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CategoryCollectionName { get; set; }
+        public string DiscountCollectionName { get; set; }
+        public string FeatureCollectionName { get; set; }
+        public string ProductCollectionName { get; set; }
+        public string SellingCollectionName { get; set; }
+    }
+}
