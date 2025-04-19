77ab46d34e3529be8c95925a833bdd3a4cd64666
emredaglar <emre.daglar1@gmail.com>
2025-04-19 14:16:40 +0300

    added CategoryController

--- MongoSuperMarket/Program.cs
+++ MongoSuperMarket/Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.Extensions.Options;
+using MongoSuperMarket.Services;
+using MongoSuperMarket.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+builder.Services.AddScoped<ISellingService, SellingService>();
+builder.Services.AddScoped<IFeatureService, FeatureService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettingsKey"));
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 var app = builder.Build();
 
