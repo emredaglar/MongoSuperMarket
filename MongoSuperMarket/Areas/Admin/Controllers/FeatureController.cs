b66bba399341d263dd69d65e88fa7273f2d6549e
emredaglar <emre.daglar1@gmail.com>
2025-04-24 22:24:10 +0300

    completed Feature

---
+++ MongoSuperMarket/Areas/Admin/Controllers/FeatureController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoSuperMarket.Dtos.FeatureDtos;
+using MongoSuperMarket.Services;
+using System.Threading.Tasks;
+
+namespace MongoSuperMarket.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FeatureController : Controller
+    {
+        private readonly IFeatureService _featureService;
+
+        public FeatureController(IFeatureService featureService)
+        {
+            _featureService = featureService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _featureService.GetAllAsync();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult CreateFeature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            await _featureService.CreateAsync(createFeatureDto);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateFeature(string id)
+        {
+            var value = await _featureService.GetByIdAsync(id);
+            return View(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            await _featureService.UpdateAsync(updateFeatureDto);
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> DeleteFeature(string id)
+        {
+            await _featureService.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
